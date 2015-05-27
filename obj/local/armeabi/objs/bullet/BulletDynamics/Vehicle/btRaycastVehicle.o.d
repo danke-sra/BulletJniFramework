ee0a02d92bb1f840c166b474c99d2b6cee96d32b
unknown <deepak.rawat@2030001700-DRAW.sisa.samsung.com>
2015-05-27 10:18:41 -0700

    Initial commit

---
+++ obj/local/armeabi/objs/bullet/BulletDynamics/Vehicle/btRaycastVehicle.o.d
@@ -0,0 +1,227 @@
+obj/local/armeabi/objs/bullet/BulletDynamics/Vehicle/btRaycastVehicle.o: \
+ jni/BulletDynamics/Vehicle/btRaycastVehicle.cpp \
+ jni/LinearMath/btVector3.h jni/LinearMath/btQuadWord.h \
+ jni/LinearMath/btScalar.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/math.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/android/api-level.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/limits.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/limits.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/limits.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/cstdlib \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/stdlib.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/string.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/malloc.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/xlocale.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/alloca.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/strings.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/types.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/stdint.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/machine/wchar_limits.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/types.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm/types.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/types.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/int-ll64.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm/bitsperlong.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/bitsperlong.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/posix_types.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/memory.h \
+ C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/cfloat \
+ C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/assert.h \
+ jni/LinearMath/btMinMax.h jni/BulletDynamics/Vehicle/btRaycastVehicle.h \
+ jni/BulletDynamics/Dynamics/btRigidBody.h \
+ jni/LinearMath/btAlignedObjectArray.h \
+ jni/LinearMath/btAlignedAllocator.h \
+ C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/new \
+ C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/cstddef \
+ jni/LinearMath/btPoint3.h jni/LinearMath/btVector3.h \
+ jni/LinearMath/btTransform.h jni/LinearMath/btMatrix3x3.h \
+ jni/LinearMath/btQuaternion.h \
+ jni/BulletCollision/BroadphaseCollision/btBroadphaseProxy.h \
+ jni/LinearMath/btScalar.h jni/LinearMath/btAlignedAllocator.h \
+ jni/BulletCollision/CollisionDispatch/btCollisionObject.h \
+ jni/LinearMath/btMotionState.h jni/LinearMath/btTransform.h \
+ jni/BulletDynamics/ConstraintSolver/btTypedConstraint.h \
+ jni/BulletDynamics/Vehicle/btVehicleRaycaster.h \
+ jni/BulletDynamics/Vehicle/btWheelInfo.h \
+ jni/BulletDynamics/ConstraintSolver/btSolve2LinearConstraint.h \
+ jni/LinearMath/btMatrix3x3.h \
+ jni/BulletDynamics/ConstraintSolver/btJacobianEntry.h \
+ jni/LinearMath/btQuaternion.h \
+ jni/BulletDynamics/Dynamics/btDynamicsWorld.h \
+ jni/BulletCollision/CollisionDispatch/btCollisionWorld.h \
+ jni/BulletCollision/CollisionDispatch/btCollisionObject.h \
+ jni/BulletCollision/CollisionDispatch/btCollisionDispatcher.h \
+ jni/BulletCollision/BroadphaseCollision/btDispatcher.h \
+ jni/BulletCollision/NarrowPhaseCollision/btPersistentManifold.h \
+ jni/BulletCollision/NarrowPhaseCollision/btManifoldPoint.h \
+ jni/LinearMath/btTransformUtil.h \
+ jni/BulletCollision/CollisionDispatch/btManifoldResult.h \
+ jni/BulletCollision/NarrowPhaseCollision/btDiscreteCollisionDetectorInterface.h \
+ jni/BulletCollision/CollisionDispatch/btCollisionCreateFunc.h \
+ jni/BulletCollision/BroadphaseCollision/btOverlappingPairCache.h \
+ jni/BulletCollision/BroadphaseCollision/btBroadphaseInterface.h \
+ jni/BulletCollision/BroadphaseCollision/btBroadphaseProxy.h \
+ jni/LinearMath/btMinMax.h \
+ jni/BulletDynamics/ConstraintSolver/btContactConstraint.h
+
+jni/LinearMath/btVector3.h:
+
+jni/LinearMath/btQuadWord.h:
+
+jni/LinearMath/btScalar.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/math.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/limits.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/cstdlib:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/stdlib.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/string.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/malloc.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/xlocale.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/alloca.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/strings.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/types.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/stdint.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/machine/wchar_limits.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/types.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm/types.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/types.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/int-ll64.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm/bitsperlong.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/bitsperlong.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/asm-generic/posix_types.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/memory.h:
+
+C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/cfloat:
+
+C:/Users/deepak.rawat/android-ndk/platforms/android-21/arch-arm/usr/include/assert.h:
+
+jni/LinearMath/btMinMax.h:
+
+jni/BulletDynamics/Vehicle/btRaycastVehicle.h:
+
+jni/BulletDynamics/Dynamics/btRigidBody.h:
+
+jni/LinearMath/btAlignedObjectArray.h:
+
+jni/LinearMath/btAlignedAllocator.h:
+
+C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/new:
+
+C:/Users/deepak.rawat/android-ndk/sources/cxx-stl/system/include/cstddef:
+
+jni/LinearMath/btPoint3.h:
+
+jni/LinearMath/btVector3.h:
+
+jni/LinearMath/btTransform.h:
+
+jni/LinearMath/btMatrix3x3.h:
+
+jni/LinearMath/btQuaternion.h:
+
+jni/BulletCollision/BroadphaseCollision/btBroadphaseProxy.h:
+
+jni/LinearMath/btScalar.h:
+
+jni/LinearMath/btAlignedAllocator.h:
+
+jni/BulletCollision/CollisionDispatch/btCollisionObject.h:
+
+jni/LinearMath/btMotionState.h:
+
+jni/LinearMath/btTransform.h:
+
+jni/BulletDynamics/ConstraintSolver/btTypedConstraint.h:
+
+jni/BulletDynamics/Vehicle/btVehicleRaycaster.h:
+
+jni/BulletDynamics/Vehicle/btWheelInfo.h:
+
+jni/BulletDynamics/ConstraintSolver/btSolve2LinearConstraint.h:
+
+jni/LinearMath/btMatrix3x3.h:
+
+jni/BulletDynamics/ConstraintSolver/btJacobianEntry.h:
+
+jni/LinearMath/btQuaternion.h:
+
+jni/BulletDynamics/Dynamics/btDynamicsWorld.h:
+
+jni/BulletCollision/CollisionDispatch/btCollisionWorld.h:
+
+jni/BulletCollision/CollisionDispatch/btCollisionObject.h:
+
+jni/BulletCollision/CollisionDispatch/btCollisionDispatcher.h:
+
+jni/BulletCollision/BroadphaseCollision/btDispatcher.h:
+
+jni/BulletCollision/NarrowPhaseCollision/btPersistentManifold.h:
+
+jni/BulletCollision/NarrowPhaseCollision/btManifoldPoint.h:
+
+jni/LinearMath/btTransformUtil.h:
+
+jni/BulletCollision/CollisionDispatch/btManifoldResult.h:
+
+jni/BulletCollision/NarrowPhaseCollision/btDiscreteCollisionDetectorInterface.h:
+
+jni/BulletCollision/CollisionDispatch/btCollisionCreateFunc.h:
+
+jni/BulletCollision/BroadphaseCollision/btOverlappingPairCache.h:
+
+jni/BulletCollision/BroadphaseCollision/btBroadphaseInterface.h:
+
+jni/BulletCollision/BroadphaseCollision/btBroadphaseProxy.h:
+
+jni/LinearMath/btMinMax.h:
+
+jni/BulletDynamics/ConstraintSolver/btContactConstraint.h:
